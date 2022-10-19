0f131f5f50a74f363dfff26c51d9be4589468715
Luka Hartwig <mail@lukahartwig.de>
2022-10-19 20:34:36 +0200

    feat: switch to google fonts

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -4,7 +4,10 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head>
-        <link rel="stylesheet" href="https://rsms.me/inter/inter.css" />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Inter&display=optional"
+          rel="stylesheet"
+        />
       </Head>
       <body>
         <Main />
