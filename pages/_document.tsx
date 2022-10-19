4978dd8ffec00894117cc5e0fc2853dd9c10f6a0
Luka Hartwig <mail@lukahartwig.de>
2022-10-19 23:14:26 +0200

    feat: add layout

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -2,14 +2,14 @@ import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
-    <Html lang="en">
+    <Html lang="en" className="h-full">
       <Head>
         <link
           href="https://fonts.googleapis.com/css2?family=Inter&display=optional"
           rel="stylesheet"
         />
       </Head>
-      <body>
+      <body className="h-full">
         <Main />
         <NextScript />
       </body>
