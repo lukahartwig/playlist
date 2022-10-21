e08dbdda51f099d51ec2c73f34e59f20d95376c2
Luka Hartwig <mail@lukahartwig.de>
2022-10-21 20:47:18 +0200

    fix: exclude is-what from dynamic check

--- src/pages/api/trpc/[trpc].ts
+++ src/pages/api/trpc/[trpc].ts
@@ -4,6 +4,9 @@ import { appRouter } from "@/server/routers/_app";
 
 export const config = {
   runtime: "experimental-edge",
+  unstable_allowDynamic: [
+    "/node_modules/.pnpm/is-what@4.1.7/node_modules/is-what/dist/index.es.js",
+  ],
 };
 
 export default async function handler(req: NextRequest) {
