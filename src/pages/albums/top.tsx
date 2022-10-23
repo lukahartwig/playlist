333e7fa310fd74fe83c1668ff77e7f312c051e6b
Luka Hartwig <mail@lukahartwig.de>
2022-10-23 19:44:00 +0200

    feat: add page title for top albums and artists

--- src/pages/albums/top.tsx
+++ src/pages/albums/top.tsx
@@ -1,15 +1,25 @@
-import { MostPlayedAlbums } from "@/components/MostPlayedAlbums";
+import Head from "next/head";
 import { useRouter } from "next/router";
+import { MostPlayedAlbums } from "@/components/MostPlayedAlbums";
 
 export default function TopArtistsPage() {
   const router = useRouter();
   const page = Number(router.query.page ?? 1);
 
   return (
-    <MostPlayedAlbums
-      page={page}
-      nextHref={`/albums/top?page=${page + 1}`}
-      prevHref={`/albums/top?page=${page > 1 ? page - 1 : 1}`}
-    />
+    <>
+      <Head>
+        <title>Playlist | Top Artists</title>
+        <meta
+          name="description"
+          content="My top albums on Spotify based on playtime."
+        />
+      </Head>
+      <MostPlayedAlbums
+        page={page}
+        nextHref={`/albums/top?page=${page + 1}`}
+        prevHref={`/albums/top?page=${page > 1 ? page - 1 : 1}`}
+      />
+    </>
   );
 }
